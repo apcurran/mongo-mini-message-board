dd9313920d96988d2ceb3d2ad574a626335bc6c7
Alex Curran <alexcurran.va@gmail.com>
2020-08-11 14:52:50 -0500

    Added post route functionality and formattedDate

--- routes/messages.js
+++ routes/messages.js
@@ -5,10 +5,10 @@ const router = express.Router();
 const { Connection } = require("../db/db-init");
 
 router.get("/", async (req, res) => {
-    // const pizza = await Connection.db.collection("pizzas").find().toArray();
-    // console.log(pizza);
+    const messages = await Connection.db.collection("messages").find().toArray();
+    console.log(messages);
 
-    res.render("index", { title: "Mongo Message" });
+    res.render("index", { title: "Mongo Message", messages: messages });
 });
 
 router.get("/new-message", (req, res) => {
@@ -18,8 +18,19 @@ router.get("/new-message", (req, res) => {
 router.post("/new-message", async (req, res) => {
     const { name, message } = req.body;
 
-    console.log(name, message);
-    res.end();
+    const date = new Date();
+    const options = { weekday: "long", year: "numeric", month: "long", day: "numeric", hour: "numeric", minute: "numeric" };
+    const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
+
+    const newMessage = {
+        name,
+        message,
+        createdAt: formattedDate
+    };
+
+    await Connection.db.collection("messages").insertOne(newMessage);
+
+    res.redirect("/");
 });
 
 module.exports = router;
