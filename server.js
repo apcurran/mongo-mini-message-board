71ed200cc978ff44fbc728bd832e1d65696b12e0
Alex Curran <alexcurran.va@gmail.com>
2021-11-19 10:39:37 -0600

    Exchange compression middleware for modern shrink-ray-current middleware to allow for brotli text compression

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 // DB Connection
 const { Connection } = require("./db/db-init");
-const compression = require("compression");
+const shrinkRay = require("shrink-ray-current");
 const helmet = require("helmet");
 const eta = require("eta");
 // Import routes
@@ -17,7 +17,7 @@ const messagesRouter = require("./routes/messages");
 app.engine("eta", eta.renderFile);
 app.set("view engine", "eta");
 app.set("views", "./views");
-app.use(compression());
+app.use(shrinkRay());
 app.use(helmet());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
